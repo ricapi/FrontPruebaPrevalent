d8f053ee8b0c28f57f5b0a8252500fa99435ae69
Ricardo Pineda <rikaldero@hotmail.com>
2021-12-16 17:22:15 -0500

    navegación tabla

--- src/components/ButtonLoading.jsx
+++ src/components/ButtonLoading.jsx
@@ -7,7 +7,7 @@ const ButtonLoading = ({ disabled, loading, text, onClick = () => { } }) => {
             onClick={onClick}
             disabled={disabled}
             type='submit'
-            className='bg-indigo-700 text-white font-bold text-lg py-3 px-6  rounded-xl hover:bg-indigo-500 shadow-md my-2 disabled:opacity-50 disabled:bg-gray-700'
+            className='bg-purple-800 text-white font-bold text-lg py-3 px-6  rounded-xl hover:bg-purple-500 shadow-md m-2 disabled:opacity-50 disabled:bg-gray-700'
         >
             {loading ? <ReactLoading type='spin' height={30} width={30} /> : text}
         </button>
--- src/pages/empresas/Index.jsx
+++ src/pages/empresas/Index.jsx
@@ -1,16 +1,15 @@
 import React, { useEffect, useState } from 'react'
-import {  useQuery } from '@apollo/client'
-import { GET_EMPRESAS} from '../../graphql/empresas/queries'
+import { useQuery } from '@apollo/client'
+import { GET_EMPRESAS } from '../../graphql/empresas/queries'
 import { Link, useParams } from 'react-router-dom';
 import { Button, Dialog, DialogContent, Icon } from '@mui/material';
 import ButtonLoading from '../../components/ButtonLoading'
 import useFormData from '../../hooks/useFormData';
-import Input from '../../components/Input';
 import Formulario from '../empresas/Formulario'
 
 
 const CreacionEmpresas = () => {
-    const { data, error, loading } = useQuery(GET_EMPRESAS);
+    const { data, error, loading } = useQuery(GET_EMPRESAS, { refetchQueries: [{ query: GET_EMPRESAS }], });
     const [openForm, setOpenForm] = useState(false);
 
     useEffect(() => {
@@ -29,7 +28,7 @@ const CreacionEmpresas = () => {
             <div className="max-w-sm rounded overflow-hidden shadow-lg content-center">
             </div>
             <div className="p-5 flex w-full sm:h-full sm:w-full xl:w-full content-center ">
-                <table className="border flex-col w-full sm:w-full sm:h-full ">
+                <table className="border flex-col sm:w-full sm:h-full lg:w-full lg:h-full md:w-full md:h-full ">
                     <thead className="min-w-full leading-normal border-gray-300">
                         <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">Nombre</th>
                         <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">Razón Social</th>
@@ -37,7 +36,6 @@ const CreacionEmpresas = () => {
                         <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">Identificación</th>
                         <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">Num epleados</th>
                         <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">Logo</th>
-                        <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">Edición</th>
                     </thead>
                     <tbody className="border-gray-300">
                         {
@@ -47,20 +45,29 @@ const CreacionEmpresas = () => {
                                         data && data.Creaciones.map((ce) => {
                                             return (
                                                 <tr key={ce._id}>
-
-                                                    <td onClick={() => { setOpenForm(true) }} className="px-5 py-5 border-b border-gray-200 bg-white text-sm">{ce.nombre}</td>
-                                                    <td onClick={() => { setOpenForm(true) }} className="px-5 py-5 border-b border-gray-200 bg-white text-sm">{ce.razonSocial}</td>
-                                                    <td onClick={() => { setOpenForm(true) }} className="px-5 py-5 border-b border-gray-200 bg-white text-sm">{ce.nit}</td>
-                                                    <td onClick={() => { setOpenForm(true) }} className="px-5 py-5 border-b border-gray-200 bg-white text-sm">{ce.identificacion}</td>
-                                                    <td onClick={() => { setOpenForm(true) }} className="px-5 py-5 border-b border-gray-200 bg-white text-sm">{ce.numEmpleados}</td>
-
-                                                    <td onClick={() => { setOpenForm(true) }} className="px-5 py-5 border-b border-gray-200 bg-white text-sm">{ce.logo}</td>
                                                     <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
                                                         <Link to={`/empresas/formulario/${ce._id}`}>
-                                                            <i className=' fa fa-edit text-yellow-600 hover:text-yellow-400 text-center cursor-pointer mx-2' />
-
+                                                            {ce.nombre}
+                                                        </Link></td>
+                                                    <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                                        <Link to={`/empresas/formulario/${ce._id}`}>
+                                                            {ce.razonSocial}
                                                         </Link>
                                                     </td>
+                                                    <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                                        <Link to={`/empresas/formulario/${ce._id}`}>
+                                                            {ce.nit}
+                                                        </Link></td>
+                                                    <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                                        <Link to={`/empresas/formulario/${ce._id}`}>
+                                                            {ce.identificacion}
+                                                        </Link></td>
+                                                    <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                                        <Link to={`/empresas/formulario/${ce._id}`}>
+                                                            {ce.numEmpleados}
+                                                        </Link></td>
+                                                    <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">{ce.logo}</td>
+
                                                 </tr>
                                             )
                                         })
